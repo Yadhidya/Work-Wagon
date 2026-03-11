d2e4e3030155dafccd98fd45a5efdd41c8250c30
Yadhidya <ulliyadhidya6002@gmail.com>
2026-03-11 19:57:53 +0530

    Build created for the backend

--- backend/src/main/java/com/backend/work_wagon/Controller/WorkerController.java
+++ backend/src/main/java/com/backend/work_wagon/Controller/WorkerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@CrossOrigin(origins = "*")
 public class WorkerController {
 
     @Autowired
